0152b2820339878b6513cf1de48db6251cf7176c
RemasAlahmari <ralahmari0031@stu.kau.edu.sa>
2026-08-23 19:12:37 +0300

    Update portfolio changes

--- js/media.js
+++ js/media.js
@@ -26,11 +26,36 @@ import { clients } from "./data.js";
 const clientVideoUrls = {
   "SEF_1.MOV":
     "https://res.cloudinary.com/otquvk5k/video/upload/v1787229013/SEF_1.mov",
+  "SEF_2.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787420344/SEF_2.mov",
+  "SEF_3.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787420251/SEF_3.mov",
+  "SEF_4.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787419938/SEF_4.mov",
+  "SEF_5.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787420237/SEF_5.mov",
+  "SEF_6.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787420097/SEF_6.mov",
+
+  "DIR_1.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787422201/DIR_1.mov",
+
+  "QID_1.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787421039/QID_1.mov",
+
+  "TPAC_1.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787418114/TPAC_1.mov",
+
+  "MOF_HAJJ_1.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787425112/mof-hajj_1.mov",
+
+  "SFA_1.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787424345/SFA_1.mov",
 
   // Add the rest of the videos here as you upload them:
   //
-  // "SEF_2.MOV": "CLOUDINARY_URL",
-  // "SEF_3.MOV": "CLOUDINARY_URL",
+  // "DIR_2.MOV": "CLOUDINARY_URL",
+  // "QID_2.MOV": "CLOUDINARY_URL",
 };
 
 // ------------------------------------------------------
@@ -38,9 +63,16 @@ const clientVideoUrls = {
 // ------------------------------------------------------
 
 const highlightVideoUrls = {
-  // Example:
+  // NOTE: only one highlight URL was provided so far, and it
+  // wasn't labeled with a specific HL number â€” this has been
+  // mapped to HL_1 (the featured highlight shown on the
+  // homepage) as the most likely intent. Let me know if this
+  // should point to a different highlight instead.
+  "HL_1.MOV":
+    "https://res.cloudinary.com/otquvk5k/video/upload/v1787424345/SFA_1.mov",
+
+  // Add the rest as you upload them:
   //
-  // "HL_1.MOV": "CLOUDINARY_URL",
   // "HL_2.MOV": "CLOUDINARY_URL",
 };
 
